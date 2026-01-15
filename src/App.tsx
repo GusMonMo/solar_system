eee368c467c35e3979ec0a465e116d8235330259
Gustavo <gusmonmodev@gmail.com>
2026-01-15 14:15:55 -0400

    start

---
+++ src/App.tsx
@@ -0,0 +1,253 @@
+import { useEffect, useRef } from 'react'
+import * as THREE from "three";
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+import camera from './hooks/camera';
+import skyMap from "./assets/images/starSky.webp"
+
+function App() {
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (!containerRef.current) return
+
+      const getSize = () => ({
+      width: window.innerWidth,
+      height: window.innerHeight,
+      });
+
+    const { width, height } = getSize();
+
+    // Renderer Config
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(width, height);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.shadowMap.enabled = true
+    containerRef.current.appendChild(renderer.domElement);
+
+    
+    const scene = new THREE.Scene();
+    scene.fog = new THREE.Fog(0xffffff, 0, 500)
+    const axesHelper = new THREE.AxesHelper(75);
+    const gridHelper = new THREE.GridHelper(150, 30)
+    const cubeTextureLoader = new THREE.CubeTextureLoader()
+    scene.background = cubeTextureLoader.load([
+      skyMap,
+      skyMap,
+      skyMap,
+      skyMap,
+      skyMap,
+      skyMap
+    ])
+    
+    // scene.add(axesHelper);
+    // scene.add(gridHelper);
+    
+    // Camera Config
+    const orbit = new OrbitControls(camera, renderer.domElement)
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+    camera.position.set(0, 2, 5);
+    orbit.minDistance = 0;
+    orbit.maxDistance = 150; 
+    orbit.enableDamping = false;
+    orbit.dampingFactor = 0.1;
+    orbit.enablePan = true
+    orbit.update()
+    
+    const handleResize = () => {
+      const { width, height } = getSize();
+      
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+      
+      renderer.setSize(width, height);
+      renderer.render(scene, camera);
+    };
+    
+    window.addEventListener('resize', handleResize);
+
+    // PlanetRefs
+    let Sun: THREE.Group | null = null;
+    let Mercury: THREE.Group | null = null;
+    let Venus: THREE.Group | null = null;
+    let Earth: THREE.Group | null = null;
+    let Mars: THREE.Group | null = null;
+    let Jupiter: THREE.Group | null = null;
+    let Saturn: THREE.Group | null = null;
+    let Uranus: THREE.Group | null = null;
+    let Neptune: THREE.Group | null = null;
+
+    //URLS
+    const assetLoader = new GLTFLoader()
+    const SunURL = new URL("./objects/sun-converted.glb", import.meta.url)
+    const MercuryURL = new URL("./objects/mercury-converted.glb", import.meta.url)
+    const VenusURL = new URL("./objects/venus-converted.glb", import.meta.url)
+    const EarthURL = new URL("./objects/earth-converted.glb", import.meta.url)
+    const MarsURL = new URL("./objects/mars-converted.glb", import.meta.url)
+    const JupiterURL = new URL("./objects/jupiter-converted.glb", import.meta.url)
+    const SaturnURL = new URL("./objects/saturn-converted.glb", import.meta.url)
+    const UranusURL = new URL("./objects/uranus-converted.glb", import.meta.url)
+    const NeptuneURL = new URL("./objects/neptune-converted.glb", import.meta.url)
+
+    // Sun
+    assetLoader.load(SunURL.href, function(glb){
+      Sun = glb.scene
+
+      scene.add(Sun)
+      Sun.scale.setScalar(0.5)
+      Sun.position.set(0,0,0)
+
+      const sunLight = new THREE.PointLight(0xffa300, 2, 0, 2);
+      scene.add(sunLight);
+    
+      sunLight.position.copy(Sun.position);
+      sunLight.intensity = 400
+      
+    }, undefined, function(error){
+      console.error(error)
+    })
+
+    //Mercury
+    assetLoader.load(MercuryURL.href, function(glb){
+      Mercury = glb.scene
+
+      if (Sun) Sun.add(Mercury)
+      Mercury.scale.setScalar(0.02)
+      Mercury.position.set(20,0,0)
+      Mercury.castShadow = true
+      
+    }, undefined, function(error){
+      console.error(error)
+    })
+
+    //Venus
+    assetLoader.load(VenusURL.href, function(glb){
+      Venus = glb.scene
+
+      if (Sun) Sun.add(Venus)
+      Venus.scale.setScalar(1.5)
+      Venus.position.set(-5,0,26)
+      Venus.castShadow = true
+      
+    }, undefined, function(error){
+      console.error(error)
+    })
+    //Earth
+    assetLoader.load(EarthURL.href, function(glb){
+      Earth = glb.scene
+
+      if (Sun) Sun.add(Earth)
+      Earth.scale.setScalar(0.025)
+      Earth.position.set(30,0,-8)
+      Earth.castShadow = true
+      
+    }, undefined, function(error){
+      console.error(error)
+    })
+    //Mars
+      assetLoader.load(MarsURL.href, function(glb){
+      Mars = glb.scene
+
+      if (Sun) Sun.add(Mars)
+      Mars.scale.setScalar(0.03)
+      Mars.position.set(-36,0,-12)
+      Mars.castShadow = true
+      
+    }, undefined, function(error){
+      console.error(error)
+    })
+    //Jupiter
+      assetLoader.load(JupiterURL.href, function(glb){
+      Jupiter = glb.scene
+
+      if (Sun) Sun.add(Jupiter)
+      Jupiter.scale.setScalar(0.04)
+      Jupiter.position.set(40, 0, 12)
+      Jupiter.castShadow = true
+      
+    }, undefined, function(error){
+      console.error(error)
+    })
+    //Saturn
+    assetLoader.load(SaturnURL.href, function(glb){
+      Saturn = glb.scene
+
+      if (Sun) Sun.add(Saturn)
+      Saturn.scale.setScalar(0.005)
+      Saturn.position.set(-9, 0, -49)
+      Saturn.castShadow = true
+      
+    }, undefined, function(error){
+      console.error(error)
+    })
+    //Uranus
+    assetLoader.load(UranusURL.href, function(glb){
+      Uranus = glb.scene
+
+      if (Sun) Sun.add(Uranus)
+      Uranus.scale.setScalar(0.008)
+      Uranus.position.set(-58, 0, -27)
+      Uranus.castShadow = true
+      
+    }, undefined, function(error){
+      console.error(error)
+    })
+    //Neptune
+    assetLoader.load(NeptuneURL.href, function(glb){
+      Neptune = glb.scene
+
+      if (Sun) Sun.add(Neptune)
+      Neptune.scale.setScalar(0.03)
+      Neptune.position.set(60, 0, -7)
+      Neptune.castShadow = true
+      
+    }, undefined, function(error){
+      console.error(error)
+    })
+
+
+    function Animation(time : any){
+
+      if (Sun) Sun.rotation.y += 0.001;        
+      if (Mercury) Mercury.rotation.y += 0.004; 
+      if (Venus) Venus.rotation.y -= 0.002;     
+      if (Earth) Earth.rotation.y += 0.003;     
+      if (Mars) Mars.rotation.y += 0.003;       
+      if (Jupiter) Jupiter.rotation.y += 0.008;
+      if (Saturn) Saturn.rotation.y += 0.007;   
+      if (Uranus) Uranus.rotation.y -= 0.006;   
+      if (Neptune) Neptune.rotation.y += 0.005;
+
+     
+      renderer.render(scene, camera);
+    }
+
+
+    renderer.setAnimationLoop(Animation);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      renderer.dispose();
+      if (containerRef.current?.contains(renderer.domElement)) {
+        containerRef.current.removeChild(renderer.domElement);
+      }
+    };
+
+  }, [])
+
+
+  return (
+    <section className='w-full min-w-screen h-full min-h-screen relative flex flex-col fullcentral'>
+      <header className='w-[80%] h-auto absolute flex fullcentral top-[1vh] left-1/2 -translate-x-1/2 
+      bg-blue-900/40 backdrop-blur-[0.5vh] border-white border-[0.5vh] rounded-full py-[1vh]'>
+        <h1 className='font-extrabold text-white'>The Solar System</h1>
+      </header>
+      <main className='w-full h-full flex fullcentral'>
+        <div ref={containerRef} className='w-full h-full'/>
+      </main>
+    </section>
+  )
+}
+
+export default App
